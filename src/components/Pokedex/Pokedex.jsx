7a4b82b68da9275be998536b438ecafafb5ca9f7
avishek195 <avishekmistry420@gmail.com>
2024-03-26 08:48:27 +0530

    Adding prev and next functions

--- src/components/Pokedex/Pokedex.jsx
+++ src/components/Pokedex/Pokedex.jsx
@@ -4,41 +4,64 @@ import { useEffect, useState } from "react";
 import "./Pokedex.css";
 import PokemonList from "../PokemonList/PokemonList";
 
+const POKE_URL = "https://pokeapi.co/api/v2/pokemon";
 const Pokedex = () => {
+  const [pokemonUrl, setPokemonUrl] = useState(POKE_URL);
   const [pokemon, setPokemon] = useState([]);
   const [next, setNext] = useState("");
+  const [prev, setPrev] = useState("");
   const getPokemon = async () => {
-    const pokeDetails = await axios.get(
-      "https://pokeapi.co/api/v2/pokemon?limit=50"
-    );
+    const pokeDetails = await axios.get(pokemonUrl);
 
     const eachDetails = pokeDetails.data.results;
-
+    setNext(pokeDetails.data.next);
+    setPrev(pokeDetails.data.previous);
     const poke = await axios.all(
       eachDetails.map(async (e) => await axios.get(e.url))
     );
-    console.log(poke);
+    console.log(pokeDetails.data);
+    const obj = [];
     poke.map((d) => {
-      setPokemon((prev) => [
-        ...prev,
-        {
-          id: d.data.id,
-          name: d.data.name,
-          img:
-            d.data.sprites.other.dream_world.front_shiny ||
-            d.data.sprites.front_shiny ||
-            d.data.sprites.front_default ||
-            d.data.sprites.back_default ||
-            d.data.sprites.back_shiny,
-        },
-      ]);
+      obj.push({
+        id: d.data.id,
+        name: d.data.name,
+        img:
+          d.data.sprites.other.dream_world.front_shiny ||
+          d.data.sprites.front_shiny ||
+          d.data.sprites.front_default ||
+          d.data.sprites.back_default ||
+          d.data.sprites.back_shiny,
+      });
+      // setPokemon((prev) => [
+      //   ...prev,
+      //   {
+      //     id: d.data.id,
+      //     name: d.data.name,
+      //     img:
+      //       d.data.sprites.other.dream_world.front_shiny ||
+      //       d.data.sprites.front_shiny ||
+      //       d.data.sprites.front_default ||
+      //       d.data.sprites.back_default ||
+      //       d.data.sprites.back_shiny,
+      //   },
+      // ]);
+      setPokemon(obj);
     });
+
+    console.log(obj);
   };
-  const changeNext = () => {};
+
+  const changeNext = () => {
+    setPokemonUrl(next);
+    // setPokemon([]);
+  };
+  const changePrev = () => {
+    setPokemonUrl(prev);
+  };
 
   useEffect(() => {
     getPokemon();
-  }, []);
+  }, [pokemonUrl]);
   return (
     <div className="main-container">
       <div className="pokedex-wrapper">
@@ -47,7 +70,13 @@ const Pokedex = () => {
         <PokemonList pokemon={pokemon} />
       </div>
       <div className="btns">
-        <button className="prev btn">Prev</button>
+        <button
+          className="prev btn"
+          disabled={prev ? false : true}
+          onClick={changePrev}
+        >
+          Prev
+        </button>
         <button className="next btn" onClick={changeNext}>
           Next
         </button>
--- src/components/PokemonCard/PokemonCard.jsx
+++ src/components/PokemonCard/PokemonCard.jsx
@@ -1,9 +1,9 @@
 const PokemonCard = ({ id, img, name }) => {
   return (
     <div className="card" key={id}>
-      <div className="pics">
-        <img src={img} alt={name} />
-      </div>
+      {/* <div className="pics"> */}
+      <img src={img} className="pokemon-img" alt={name} />
+      {/* </div> */}
       <h3>{name.toUpperCase()}</h3>
     </div>
   );
